54a9745d01e2534fa652929ff26d63e3166f4b71
ahmadfadillllah <ahmadfadillah502@gmail.com>
2022-12-07 16:03:20 +0800

    Update perhitungan

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -21,7 +21,7 @@
             </div>
         </div>
     </div>
-    <div class="row">
+    {{-- <div class="row">
         <div class="col-md-12 col-lg-4">
             <div class="card">
                 <div class="card-body">
@@ -195,7 +195,7 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> --}}
 </div>
 <!-- Content Wrapper END -->
 @include('dashboard.layout.footer')
